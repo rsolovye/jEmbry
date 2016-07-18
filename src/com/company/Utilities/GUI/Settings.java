6528e86f48de266e86107ff3158551060f262779
rsolovye <rsolovye@gmail.com>
2016-07-18 14:38:54 +0300

    Add files via upload

---
+++ src/com/company/Utilities/GUI/Settings.java
@@ -0,0 +1,19 @@
+package com.company.Utilities.GUI;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public  class Settings {
+
+    public static  final String[] DIAGNOSES = {"97.0", "97.1", "97.2", "97.3", "97.4", "97.8"};
+    public static  final String[] HEPATITIS = {"HepA", "HepB", "HepC", "HepA", "HepB", "HepC"};
+    public static  final String[] DOCTORS = {"Гвасалия Р.Г.", "Шарфи Ю.Н.", "Геркулов Д.А.", "Жарова М.В."};
+
+//Gui global vars
+
+    public static final Border simple_line_border = BorderFactory.createLineBorder(new Color(4, 212, 223), 2);
+    public static final  Color default_background = new Color(72, 188, 228);
+    public static final Color default_border_color = new Color(4, 212, 223);
+
+}
