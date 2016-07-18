ab712a2694b963fe5afcdc61e32d39d9164e0c8f
rsolovye <rsolovye@gmail.com>
2016-07-18 14:52:00 +0300

    ss

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Новая Пункция");
+
+        NextDayPF ndpf = new NextDayPF();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(ndpf.getPanel());
+    frame.pack();
+        frame.setVisible(true);
+
+	// write your code here:s
+    }
+}
